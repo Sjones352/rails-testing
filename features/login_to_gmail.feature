e1b3b91aee53295fdc68968fede8f974bce95b1d
Sjones352 <sjones@tistatech.com>
2018-10-13 13:41:11 -0400

    feature definitions

--- features/login_to_gmail.feature
+++ features/login_to_gmail.feature
@@ -3,6 +3,9 @@
 Feature: Login Action to gmail
 
 Scenario: Successful Login with Valid Credentials
-	Given User navigates to www.gmail.com
-	When User logins using email as "jonessandra708@gmail.com" and password "KaeLynn10**"
-	Then User should be able to login
+	Given I navigate to www.gmail.com
+	When  I login using email as "jonessandra708@gmail.com"
+	Then I click buttton "Next"
+	When I enter password as  "KaeLynn10**"
+	Then I click buttton "Next"
+	Then I should be able to login
--- features/step_definitions/login_test.rb
+++ features/step_definitions/login_test.rb
@@ -1,20 +1,24 @@
 require 'rubygems';
 require 'watir-webdriver';
-require 'colorize';
 
-# driver = Selenium::WebDriver.for :chrome
 browser = Watir::Browser.new :chrome
 
-Given(/^User navigates to www\.gmail\.com$/) do
+Given(/^I navigate to www\.gmail\.com$/) do
   browser.goto "www.gmail.com"
 end
 
-When(/^User logins using email as "([^"]*)" and password "([^"]*)"$/) do |arg1, arg2|
-  brower.input(:name, "identifier").set("jonessandra708@gmail.com")
-  browser.button(:id, "identifierNext").click
+When(/^I login using email as "([^"]*)"$/) do |arg1|
+  browser.input(:name, "identifier")
 end
 
-Then(/^User should be able to login$/) do
+Then(/^I click buttton "([^"]*)"$/) do |arg1|
+  browser.button(:id, "identifierNext")
+end
+
+When(/^I enter password as  "([^"]*)"$/) do |arg1|
+  browser.input(:name, "password")
+end
+
+Then(/^I should be able to login$/) do
   puts "Successful Logged in"
-  broswer.close
 end
